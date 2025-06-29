ac81c95d8536bef940f5c91e3ff62f6c4d03579b
Molhamo <mlm54123@gmail.com>
2025-06-29 14:27:07 +0300

    Added feature to select status effect type

--- Program.py
+++ Program.py
@@ -9,6 +9,12 @@ match Ball:
         BallRate = 2
 Health = int(input("What is the pokemon's health percentage?"))
 Health = Health / 100
-StatusRate = int(input("What is the catch rate multiplier from status effects?"))
+Status = input("What is the pokemon's status effect?")
+if(Status == "sleep" or Status == "freeze"):
+    StatusRate = 2.5
+elif(Status == "paralyze" or Status == "poison" or Status == "burn"):
+    StatusRate = 1.5
+else:
+    StatusRate = 1
 CatchRate = ((MonRate*BallRate*StatusRate*(1-((2.0/3.0)*Health)))/255)*100
 print(f"The current catch rate is {CatchRate}%")
